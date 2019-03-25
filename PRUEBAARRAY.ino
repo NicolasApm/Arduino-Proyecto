d19f14b49a2c5c8829cb1a6733a0af443633f9d7
stenicolasap@gmail.com <42557909+NicolasApm@users.noreply.github.com>
2019-03-25 15:10:58 -0500

    cambios 25032019

--- PRUEBAARRAY.ino
+++ PRUEBAARRAY.ino
@@ -10,10 +10,11 @@ String Error = "ErrorLength";
 String data [dataLength];
 String SerialString = "";
 const char separator = ',';
+char array2[32];
+char My_Array[16];
 int b = 0;
-//int data[dataLength];
+int count = 0;
 
-char VarChar;
 void setup()
 {
   ModBluetooth.begin(9600);
@@ -22,10 +23,52 @@ void setup()
 
 void loop()
 {
+  if (Serial.available() > 0) //something is there to be read
+  {
+GameInsertData();
+  }
 
   if (ModBluetooth.available()) {
     GameCompare();
   }
+}
+
+
+void GameInsertData() {
+  char C = Serial.read();
+
+  if (C == '.')
+  {
+    int i = 0;
+    /*while (i != count)
+      {
+      //Serial.print(My_Array[i++]);
+      My_Array[i++];
+      }*/
+
+    for (int i = 0; i < sizeof(My_Array); i++) {
+      //Serial.print(My_Array[i]);
+    }
+    int b = 0;
+    for (int i = 0; i < count; i++) {
+
+
+      array2[b++] = My_Array[i];
+      array2[b++] = ',';
+
+    }
+    //  for (int i = 0; i < sizeof(array2); i++) {
+    //Serial.print(array2[i-1]);
+    //}
+    for (int i = 0; i < (count * 2) - 1; i++) {
+      //Serial.print(array2[i]);
+       Salida+=array2[i];
+    }
+   Serial.print(Salida);
+   count = 0;
+  }
+  else  My_Array[count++] = C;
+
 }
 
 void GameCompare() {
@@ -33,15 +76,16 @@ void GameCompare() {
   str = ModBluetooth.readString();
 
   if (str.equals("41")) {
-    String Salida = "a,f,k,p";
+   // String Salida = "a,f,k,p";
     digitalWrite(13, HIGH);
     delay(100);
     digitalWrite(13, LOW);
+    Serial.print(Salida);
     ModBluetooth.print(Salida);
     ModBluetooth.print("#");
   }
   else if (str.equals("42")) {
-    String Salida = "f,g,k,p";
+   // String Salida = "f,g,k,p";
     digitalWrite(13, HIGH);
     delay(100);
     digitalWrite(13, LOW);
@@ -49,7 +93,7 @@ void GameCompare() {
     ModBluetooth.print("#");
   }
   else if (str.equals("53")) {
-    Salida = "a,k,j,l,p";
+   // Salida = "a,k,j,l,p";
     digitalWrite(13, HIGH);
     delay(100);
     digitalWrite(13, LOW);
@@ -61,6 +105,7 @@ void GameCompare() {
     ModBluetooth.print(Error);
     ModBluetooth.print("#");
   }
+  Salida="";
 
   //else{DEBUG_ARRAY("eroor")}
 
